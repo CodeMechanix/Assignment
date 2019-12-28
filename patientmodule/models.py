2b6058fe95d6c7e78321a277d1475c2c2aa090fc
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2019-12-28 22:24:59 +0600

    set arbitary length of variable

--- patientmodule/models.py
+++ patientmodule/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class DoctorDetails(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.TextField()
 
     def __str__(self):
         return self.name

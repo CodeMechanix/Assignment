e3cc27fe3b68bfe0c2b820c335b08be9dd0f4f38
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2019-12-29 23:22:23 +0600

    setup ttl in table

--- patientmodule/models.py
+++ patientmodule/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
+import datetime
 
 class DoctorDetails(models.Model):
     name = models.TextField()
@@ -13,6 +15,21 @@ class PatientDetails(models.Model):
     patient_address = models.TextField()
 
     doctor = models.ForeignKey('DoctorDetails', on_delete=models.CASCADE)
+
+    publishing_date = models.DateTimeField(
+        default=timezone.now,
+        blank=True,
+    )
+    # Set TTL for Five Minutes
+    @property
+    def delete_after_five_minutes(self):
+        time = self.publishing_date + datetime.timedelta(minutes=5)
+        if time < datetime.datetime.now():
+            e = PatientDetails.objects.get(pk=self.pk)
+            e.delete()
+            return True
+        else:
+            return False
 
     def __str__(self):
         return self.patient_name
